039bca040e2032a3b96dd1674bc03a6668e2df1b
chrisryda <chrisryda@hotmail.com>
2025-09-16 11:06:55 +0200

    Docs

--- configs/sic_parvis_magna/sic_parvis.py
+++ configs/sic_parvis_magna/sic_parvis.py
@@ -9,19 +9,19 @@ from gem5.components.processors.base_cpu_core import BaseCPUCore
 from gem5.components.processors.base_cpu_processor import BaseCPUProcessor
 from gem5.isas import ISA
 
-# O3CPUCore extends Arn3CPU. ArmO3CPU is one of gem5's internal models
-# the implements an out of order pipeline. Please refer to
+# O3CPUCore extends Arm3CPU. ArmO3CPU is one of gem5's internal models
+# that implements an out of order pipeline. Please refer to
 # https://www.gem5.org/documentation/general_docs/cpu_models/O3CPU
 # to learn more about O3CPU.
+
+
 class O3CPUCore(ArmO3CPU):
     def __init__(self, width, rob_size, num_int_regs, num_fp_regs):
         """
-        :param width: sets the width of fetch, decode, rename, issue, wb, and
-        commit stages.
-        :param rob_size: determine the number of entries in the reorder buffer.
+        :param width:        sets the width of fetch, decode, rename, issue, wb, and commit stages.
+        :param rob_size:     determine the number of entries in the reorder buffer.
         :param num_int_regs: determines the size of the integer register file.
-        :param num_int_regs: determines the size of the vector/floating point
-        register file.
+        :param num_int_regs: determines the size of the vector/floating point register file.
         """
         super().__init__()
         self.fetchWidth = width
@@ -41,8 +41,8 @@ class O3CPUCore(ArmO3CPU):
         self.LQEntries = 32
         self.SQEntries = 32
 
-# Along with BaseCPUCore, CPUStdCore wraps CPUCore to a core compatible
-# with gem5's standard library. Please refer to
+# Along with BaseCPUCore, O3CPUStdCore wraps O3CPUCore to a core 
+# compatible with gem5's standard library. Please refer to
 # gem5/src/python/gem5/components/processors/base_cpu_core.py
 # to learn more about BaseCPUCore.
 
@@ -50,18 +50,16 @@ class O3CPUCore(ArmO3CPU):
 class O3CPUStdCore(BaseCPUCore):
     def __init__(self, width, rob_size, num_int_regs, num_fp_regs):
         """
-        :param width: sets the width of fetch, decode, raname, issue, wb, and
-        commit stages.
-        :param rob_size: determine the number of entries in the reorder buffer.
+        :param width:        sets the width of fetch, decode, raname, issue, wb, and commit stages.
+        :param rob_size:     determine the number of entries in the reorder buffer.
         :param num_int_regs: determines the size of the integer register file.
-        :param num_int_regs: determines the size of the vector/floating point
-        register file.
+        :param num_int_regs: determines the size of the vector/floating point register file.
         """
         core = O3CPUCore(width, rob_size, num_int_regs, num_fp_regs)
         super().__init__(core, ISA.ARM)
 
 
-# O3CPU along with BaseCPUProcessor wraps CPUCore to a processor
+# O3CPU along with BaseCPUProcessor wraps O3CPUCore to a processor
 # compatible with gem5's standard library. Please refer to
 #  gem5/src/python/gem5/components/processors/base_cpu_processor.py
 # to learn more about BaseCPUProcessor.
@@ -70,12 +68,10 @@ class O3CPUStdCore(BaseCPUCore):
 class O3CPU(BaseCPUProcessor):
     def __init__(self, width, rob_size, num_int_regs, num_fp_regs):
         """
-        :param width: sets the width of fetch, decode, raname, issue, wb, and
-        commit stages.
-        :param rob_size: determine the number of entries in the reorder buffer.
+        :param width:        sets the width of fetch, decode, raname, issue, wb, and commit stages.
+        :param rob_size:     determine the number of entries in the reorder buffer.
         :param num_int_regs: determines the size of the integer register file.
-        :param num_int_regs: determines the size of the vector/floating point
-        register file.
+        :param num_int_regs: determines the size of the vector/floating point register file.
         """
         cores = [O3CPUStdCore(width, rob_size, num_int_regs, num_fp_regs)]
         super().__init__(cores)
@@ -86,8 +82,8 @@ class O3CPU(BaseCPUProcessor):
 
     def get_area_score(self):
         """
-        :returns the area score of a pipeline using its parameters width,
-        rob_size, num_int_regs, and num_fp_regs.
+        :returns the area score of a pipeline using its parameters 
+        width, rob_size, num_int_regs, and num_fp_regs.
         """
         score = (
             self._width
