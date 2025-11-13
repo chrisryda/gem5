7f32f7c1f4627f6ddb295f164d7434b332c417e7
chrisryda <chrisryda@hotmail.com>
2025-11-13 11:17:32 +0100

    Changed plotting logic

--- plot_dependencies/plot_dep.py
+++ plot_dependencies/plot_dep.py
@@ -8,71 +8,62 @@ import sys
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", dest="plt_type", type=str, help="The type of plot {scatter, hist, sorted}")
-parser.add_argument("-f", dest="file_name", type=str, help="The file to plot {whet<1,5,10,15,20>}")
+parser.add_argument("-hist",  dest="hist_type",  action=argparse.BooleanOptionalAction, help="Plot histogram. Default is scatter plot.")
+parser.add_argument('-histv', dest="histv_type", action=argparse.BooleanOptionalAction, help="Plot histogram with num values on the bars.")
 parser.add_argument('-s', dest="save_plot", action=argparse.BooleanOptionalAction, help="Save plot to file")
-parser.add_argument('-v', dest="add_values", action=argparse.BooleanOptionalAction, help="Add num values to the bars in the histogram")
+parser.add_argument("-f", dest="file_name", type=str, help="The file to plot")
 args = parser.parse_args()
-
-plt_type = args.plt_type if args.plt_type else "scatter"
-file_name = args.file_name if args.file_name else "whet1B"
 
 def make_hist() -> None:
     delta_only = []
     for d, i in zip(delta, instructions):
-        for _ in range(i):
-            delta_only.append(d)
+        if d <= 100:
+            for _ in range(i):
+                delta_only.append(d)
     delta_only = np.array(delta_only)
-    counts, bins, _ = plt.hist(delta_only, bins="auto", edgecolor="black", alpha=0.9)
     
-    if args.add_values:
+    bins = np.arange(delta_only.min(), delta_only.max() + 2) - 0.5
+    counts, bins, _ = plt.hist(delta_only, bins=bins, edgecolor="black", alpha=0.9)
+    
+    if args.histv_type:
         for count, x in zip(counts, bins):
             if count != 0:
                 plt.text(
                     x + (bins[1]-bins[0])/2,  # midpoint of each bin
-                    20,                       # height (where text goes)
+                    1000,                     # height (where text goes)
                     str(int(count)),          # text label
                     ha='center', va='bottom', # text alignment
-                    rotation=90
+                    rotation=90,
+                    # fontsize=3
                 )
     return
-    
-def make_sorted() -> None:
-    sort = df[df[:, 0].argsort()]
-    x = np.array([e[0] for e in sort])
-    y = np.array([e[1] for e in sort])
-    a, b, c = np.polyfit(x, y, 2)
-    line = a*x*x + b*x + c
-    plt.plot(x, y, line, color="red")
-    return
-    
+
+file_name = args.file_name if args.file_name else "whet1B"
 df = pd.read_csv(f"plot_dependencies/stats/dist_dependencies_{file_name}.csv").to_numpy()
 delta = np.array([e[0] for e in df])
 instructions = np.array([e[1] for e in df])
-match plt_type:
-    case "scatter":
-        plt.scatter(delta, instructions)
-        plt.title(f"Scatter-plot for {file_name}")
-    case "hist":
-        make_hist()
-        plt.title(f"Histogram for {file_name}")
-    case "sorted":
-        plt.title(f"<something>-plot for {file_name}")
-        make_sorted()
-    case _:
-        print("Invalid option, exiting")
-        sys.exit(0)
+
+if args.hist_type or args.histv_type:
+    make_hist()
+    plt_type = "hist"
+    plt.title(f"Histogram for {file_name}")
+else:
+    plt.scatter(delta, instructions)
+    plt_type = "scatter"
+    plt.title(f"Scatter-plot for {file_name}")
 
 plt.xlabel("Delta (cycles)")
 plt.ylabel("Number of instructions")
+plt.xlim((-0.5, 100.5))
 plt.grid(True, linestyle="--", alpha=0.5)
 try:
     if args.save_plot:
         plt.savefig(
             f"/home/crd/Documents/y6s1/project-TDT4501/{file_name}_{plt_type}.pdf",
-            dpi=300,
+            dpi=150,
             bbox_inches="tight",
-            facecolor="white"
+            facecolor="white",
+            orientation="landscape"
         )
     plt.show()
 except KeyboardInterrupt:
