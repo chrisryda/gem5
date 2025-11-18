2fc24178d6ff3c04a22822d98bf06ca4ce9cda99
chrisryda <chrisryda@hotmail.com>
2025-11-18 12:55:42 +0100

    Added args for lbm and potential resources for ff

--- configs/sic_parvis_magna/magna.py
+++ configs/sic_parvis_magna/magna.py
@@ -1,14 +1,28 @@
 from sic_parvis import Magna
 
-from gem5.components.boards.simple_board import SimpleBoard
-from gem5.components.cachehierarchies.classic.no_cache import NoCache
-from gem5.components.memory import SingleChannelDDR3_1600
-from gem5.resources.resource import BinaryResource
+from gem5.isas import ISA
 from gem5.simulate.simulator import Simulator
+from gem5.resources.resource import BinaryResource
+
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_switchable_processor import SimpleSwitchableProcessor
 
 processor = Magna()
 memory = SingleChannelDDR3_1600(size="32MiB")
 cache_hierarchy = NoCache()
+
+# For fast-forwarding, might need it later  
+# processor = SimpleSwitchableProcessor(
+#     starting_core_type=CPUTypes.ATOMIC,
+#     switch_core_type=CPUTypes.O3,
+#     isa=ISA.ARM,
+#     num_cores=1,
+# )
+# simulator.run(x) // processor.switch() // simulator.run(y)
+
 
 board = SimpleBoard(
     clk_freq="2GHz",
@@ -18,7 +32,13 @@ board = SimpleBoard(
 )
 
 binary = "/home/crd/nec/gem5/tests/test-progs/simsim/bin/simple_for"
-board.set_se_binary_workload(BinaryResource(binary))
+# binary = "/home/crd/nec/gem5/tests/test-progs/simsim/bin/whetstone"
+# binary = "/home/crd/nec/gem5/tests/test-progs/lbm/program"
+
+args = []
+# args = ["64", "reference.dat", "0", "1", "/home/crd/nec/gem5/tests/test-progs/lbm/100_100_130_cf_a.of"]
+
+board.set_se_binary_workload(binary=BinaryResource(binary), arguments=args)
 
 simulator = Simulator(board=board)
 simulator.run()
