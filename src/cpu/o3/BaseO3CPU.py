a1b0f0708500f0dd9244c49d052f0008f7364caf
chrisryda <chrisryda@hotmail.com>
2026-04-15 12:57:08 +0200

    Made DIQ entries info like other info

--- src/cpu/o3/BaseO3CPU.py
+++ src/cpu/o3/BaseO3CPU.py
@@ -186,7 +186,7 @@ class BaseO3CPU(BaseCPU):
     # most ISAs don't use condition-code regs, so default is 0
     numPhysCCRegs = Param.Unsigned(0, "Number of physical cc registers")
     numIQEntries = Param.Unsigned(64, "Number of instruction queue entries")
-    numDeltaIQEntries = Param.Unsigned(14, "Number of delta IQ entries per thread")
+    numDeltaIQEntries = Param.Unsigned(14, "Number of delta IQ entries")
     numROBEntries = Param.Unsigned(192, "Number of reorder buffer entries")
 
     smtNumFetchingThreads = Param.Unsigned(1, "SMT Number of Fetching Threads")
