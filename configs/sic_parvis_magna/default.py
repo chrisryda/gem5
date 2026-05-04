01639df3f2207e3c61fb9d9771bf7d89bd956ca7
Christian Ryddheim Dahlin <chrisryda@hotmail.com>
2026-05-04 10:52:36 +0200

    Updated defualt.py to match magna in terms of param acceptence etc.

--- configs/sic_parvis_magna/default.py
+++ configs/sic_parvis_magna/default.py
@@ -1,62 +1,34 @@
-# Copyright (c) 2021-2025 The Regents of the University of California
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met: redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer;
-# redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution;
-# neither the name of the copyright holders nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-"""
-This gem5 configuation script creates a simple board to run an ARM
-"hello world" binary.
-
-This setup is close to the simplest setup possible using the gem5
-library. It does not contain any kind of caching, IO, or any non-essential
-components.
-"""
 import os
 import argparse
-# from sic_parvis import Magna, MagnaOpus, IceLakeCacheHierarchy
 
+import m5
 from gem5.isas import ISA
 from gem5.utils.requires import requires
+from gem5.utils.override import overrides
 from gem5.simulate.simulator import Simulator
 from gem5.resources.resource import BinaryResource
 from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.memory.simple import SingleChannelSimpleMemory
 from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_core import SimpleCore
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.abstract_processor import AbstractProcessor
+from gem5.components.processors.switchable_processor import SwitchableProcessor
+from gem5.components.boards.mem_mode import MemMode
 from gem5.components.boards.simple_board import SimpleBoard
 from gem5.components.cachehierarchies.classic.private_l1_private_l2_cache_hierarchy import PrivateL1PrivateL2CacheHierarchy
-from gem5.components.processors.simple_processor import SimpleProcessor
 
 CLK_GHZ = 3.3
 
-def get_num_ticks(ticks: str) -> int:
-    ticks = ticks.strip().upper()
-    if ticks.endswith("M"):
-        return int(float(ticks[:-1]) * 1_000_000)
-    if ticks.endswith("B"):
-        return int(float(ticks[:-1]) * 1_000_000_000)
-    if ticks.isdigit():
-        return int(ticks)
-    raise ValueError(f"Unrecognized format: {ticks}")
+def parse_count(s: str) -> int:
+    s = s.strip().upper()
+    if s.endswith("M"):
+        return int(float(s[:-1]) * 1_000_000)
+    if s.endswith("B"):
+        return int(float(s[:-1]) * 1_000_000_000)
+    if s.isdigit():
+        return int(s)
+    raise ValueError(f"Unrecognized format: {s}")
 
 def get_num_cycles(cycles: str) -> int:
     cycles = cycles.strip().upper()
@@ -71,17 +43,75 @@ def get_num_cycles(cycles: str) -> int:
     ticks_per_cycle = round(1e12 / (CLK_GHZ * 1e9))
     return num * ticks_per_cycle
 
+
+def _apply_super_params(cpu):
+    cpu.fetchWidth = 10
+    cpu.decodeWidth = 10
+    cpu.renameWidth = 10
+    cpu.dispatchWidth = 10
+    cpu.issueWidth = 12
+    cpu.wbWidth = 12
+    cpu.commitWidth = 12
+    cpu.numROBEntries = 1024
+    cpu.LQEntries = 1024
+    cpu.SQEntries = 1024
+    cpu.numPhysIntRegs = 1024
+    cpu.numPhysFloatRegs = 1024
+
+
+class DefaultSwitchableProcessor(SwitchableProcessor):
+    """ATOMIC warmup core --> simple O3 timing core."""
+
+    def __init__(self, iq_size, diq_size, super_mode=False):
+        self._start_key = "start"
+        self._switch_key = "switch"
+        self._current_is_start = True
+
+        atomic_core = SimpleCore(cpu_type=CPUTypes.ATOMIC, core_id=0, isa=ISA.ARM)
+        o3_core = SimpleCore(cpu_type=CPUTypes.O3, core_id=0, isa=ISA.ARM)
+        o3_core.get_simobject().cpu_id = atomic_core.get_simobject().cpu_id
+        o3_core.get_simobject().numIQEntries = iq_size
+        o3_core.get_simobject().numDeltaIQEntries = diq_size
+        if super_mode:
+            _apply_super_params(o3_core.get_simobject())
+
+        super().__init__(
+            switchable_cores={
+                self._start_key: [atomic_core],
+                self._switch_key: [o3_core],
+            },
+            starting_cores=self._start_key,
+        )
+
+    @overrides(SwitchableProcessor)
+    def incorporate_processor(self, board):
+        super().incorporate_processor(board=board)
+        board.set_mem_mode(MemMode.ATOMIC)
+
+    @overrides(AbstractProcessor)
+    def switch(self):
+        if self._current_is_start:
+            self._board.set_mem_mode(MemMode.TIMING)
+            self.switch_to_processor(self._switch_key)
+        else:
+            self._board.set_mem_mode(MemMode.ATOMIC)
+            self.switch_to_processor(self._start_key)
+        self._current_is_start = not self._current_is_start
+
+
 parser = argparse.ArgumentParser()
 sim_limit = parser.add_mutually_exclusive_group()
 sim_limit.add_argument("-t", dest="ticks", type=str, help="The amount of ticks to simulate")
 sim_limit.add_argument("-c", dest="cycles", type=str, help="The amount of cycles to simulate")
 parser.add_argument("-b", dest="binary", type=str, help="The benchmark to run")
+parser.add_argument("--warmup-insts", type=str, default="0", help="Instructions to fast-forward in ATOMIC mode before timing measurement (e.g. 100M, 1B)")
 parser.add_argument("--iq-size", type=int, default=64, help="Number of IQ entries")
 parser.add_argument("--diq-size", type=int, default=0, help="Number of Delta IQ entries")
+parser.add_argument("--zero-lat", action="store_true", default=False, help="Use near-zero DRAM latency to isolate IQ bottleneck")
+parser.add_argument("--super", dest="super_mode", action="store_true", default=False, help="Use over-provisioned processor (wide pipeline, large ROB/LSQ/regfile) to isolate IQ as bottleneck")
 args = parser.parse_args()
+warmup_insts = parse_count(args.warmup_insts)
 
-# This check ensures the gem5 binary contains the ARM ISA target. If not, an
-# exception will be thrown.
 requires(isa_required=ISA.ARM)
 
 cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
@@ -89,18 +119,25 @@ cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
     l1i_size="32KiB",
     l2_size="256KiB",
 )
+if args.zero_lat:
+    memory = SingleChannelSimpleMemory(latency="1ns", latency_var="0ns", bandwidth="1TiB/s", size="16GiB")
+else:
+    memory = SingleChannelDDR3_1600(size="16GiB")
 
-# We use a single channel DDR3_1600 memory system
-memory = SingleChannelDDR3_1600(size="16GiB")
+if warmup_insts > 0:
+    processor = DefaultSwitchableProcessor(iq_size=args.iq_size, diq_size=args.diq_size, super_mode=args.super_mode)
+    proc_name = "Switch Super Default O3" if args.super_mode else "Switch Default O3"
+else:
+    processor = SimpleProcessor(cpu_type=CPUTypes.O3, isa=ISA.ARM, num_cores=1)
+    core = processor.get_cores()[0].get_simobject()
+    core.numIQEntries = args.iq_size
+    core.numDeltaIQEntries = args.diq_size
+    if args.super_mode:
+        _apply_super_params(core)
+    proc_name = "Super O3" if args.super_mode else "Default O3"
 
-# We use a simple O3 processor with one core.
-processor = SimpleProcessor(cpu_type=CPUTypes.O3, isa=ISA.ARM, num_cores=1)
-processor.get_cores()[0].get_simobject().numIQEntries = args.iq_size
-processor.get_cores()[0].get_simobject().numDeltaIQEntries = args.diq_size
-
-# The gem5 library simple board which can be used to run SE-mode simulations.
 board = SimpleBoard(
-    clk_freq="3.3GHz",
+    clk_freq=f"{CLK_GHZ}GHz",
     processor=processor,
     memory=memory,
     cache_hierarchy=cache_hierarchy,
@@ -111,7 +148,7 @@ if args.cycles:
     sim_desc = f"{args.cycles} cycles"
 else:
     ticks = args.ticks if args.ticks else "1M"
-    num_ticks = get_num_ticks(ticks)
+    num_ticks = parse_count(ticks)
     sim_desc = f"{ticks} ticks"
 
 binary = args.binary if args.binary else "hello_world"
@@ -124,22 +161,28 @@ match binary:
     case "whetstone":
         binary_path = f"{test_dir}/whetstone/whetstone"
         binary_args = ["-c", "10000000000"]
-    case "lbm_s": #src folder copied from 519.lbm_r
+    case "lbm_s":
         binary_path = f"{test_dir}/619.lbm_s/lbm_s"
         binary_args = ["2000", "reference.dat", "0", "0", f"{test_dir}/619.lbm_s/data/refspeed/input/200_200_260_ldc.of"]
-    case "mcf_s": # src and data folders copied from 505.mcf_r
+    case "mcf_s":
         binary_path = f"{test_dir}/605.mcf_s/mcf_s"
         binary_args = [f"{test_dir}/605.mcf_s/data/refspeed/input/inp.in"]
-    case "gcc_s": # src and data folders copied from 502.gcc_r
+    case "gcc_s":
         binary_path = f"{test_dir}/602.gcc_s/sgcc"
         binary_args = [f"{test_dir}/602.gcc_s/data/refspeed/input/gcc-pp.c"]
 
 board.set_se_binary_workload(binary=BinaryResource(binary_path), arguments=binary_args)
 simulator = Simulator(board=board)
-print(f"Running benchmark {binary} for {sim_desc} with IQ = {args.iq_size} and DIQ = {args.diq_size}\n")
+print(f"Running benchmark {binary} for {sim_desc} with {proc_name}, IQ = {args.iq_size} and DIQ = {args.diq_size}\n")
 
-# simulator.schedule_max_insts(1_000_000_000)
-# simulator.run()
+if warmup_insts > 0:
+    print(f"\nFast-forwarding {warmup_insts:,} instructions in ATOMIC mode...\n\n")
+    simulator.schedule_max_insts(warmup_insts)
+    simulator.run()
+    m5.stats.reset()
+    simulator.switch_processor()
+    print(f"\nWarmup done after {simulator.get_current_tick()} simulated ticks. Switched CPU, starting measurements...\n\n")
+
 simulator.run(num_ticks)
 
-print(f"{binary} ran a total of {simulator.get_current_tick()} simulated ticks with IQ = {args.iq_size} and DIQ = {args.diq_size}\n")
+print(f"{binary} ran a total of {simulator.get_current_tick()} simulated ticks on {proc_name} with IQ = {args.iq_size} and DIQ = {args.diq_size}\n")
